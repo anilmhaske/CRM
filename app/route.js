25740301dff30e3b4ea43665fcdcd35bd8aa8796
anilmhaske <anil.mhaske@thadhani.info>
2018-12-04 14:43:44 +0530

    added

---
+++ app/route.js
@@ -0,0 +1,6 @@
+var app =  angular.module('retailwarecrmmanagement',['ngSanitize','ngRoute','angularUtils.directives.dirPagination']);
+//var app =  angular.module('retailwarecrmmanagement',['ngSanitize','ngRoute','angularUtils.directives.dirPagination','ui.bootstrap','long2know']);
+
+  
+
+ 
